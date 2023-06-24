f10f6f7d25fe2bae895b555080422080b9e1d594
Max-Person <max_person@mail.ru>
2023-06-25 00:57:14 +0300

    [feat] базовая реализация основного функционала меню

--- app/src/main/java/com/github/max_person/hammersystems/test/composables/MainMenuScreen.kt
+++ app/src/main/java/com/github/max_person/hammersystems/test/composables/MainMenuScreen.kt
@@ -1,10 +1,17 @@
 package com.github.max_person.hammersystems.test.composables
 
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.pm.ActivityInfo
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -15,25 +22,40 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.Photo
 import androidx.compose.material.icons.filled.QrCode
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.SubcomposeAsyncImage
+import coil.request.ImageRequest
+import com.github.max_person.hammersystems.test.data.MenuViewModel
+import com.github.max_person.hammersystems.test.data.models.FoodCard
 import me.onebone.toolbar.CollapsingToolbarScaffold
 import me.onebone.toolbar.ScrollStrategy
 import me.onebone.toolbar.rememberCollapsingToolbarScaffoldState
@@ -43,7 +65,9 @@ val horizontalPadding = 10.dp
 @Preview
 @Composable
 fun MainMenuScreen (
+    viewModel : MenuViewModel = hiltViewModel()
 ) {
+    LockScreenOrientation(orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
     Surface(
         modifier = Modifier.fillMaxSize(),
     ) {
@@ -93,29 +117,59 @@ fun MainMenuScreen (
                         )
                     },
                 ) {
-                    Column {
+                    Column(
+                        modifier = Modifier.fillMaxHeight()
+                    ) {
+
+                        val categories by remember{viewModel.categories.data}
+                        val categoriesLoading by remember{viewModel.categories.isLoading}
+                        LaunchedEffect(Unit){
+                            viewModel.categories.load(Unit)
+                        }
+
+                        var selectedCategory by remember{viewModel.selectedCategory}
+
                         val listState = rememberLazyListState()
 
-                        HorizontalButtonSelection(
-                            values = (1..10).toList().map { it.toString() },
-                            modifier = Modifier.padding(bottom = 5.dp)
-//                                .clip(GenericShape { size, _ ->
-//                                    lineTo(size.width, 0f)
-//                                    lineTo(size.width, Float.MAX_VALUE)
-//                                    lineTo(0f, Float.MAX_VALUE)
-//                                })
-//                                .shadow((if(listState.canScrollBackward) 10 else 0).dp)
-                        ) {
-
+                        val foods by remember{viewModel.foods.data}
+                        var foodsLoading by remember{viewModel.foods.isLoading}
+                        LaunchedEffect(selectedCategory){
+                            println("launched for category $selectedCategory")
+                            if(selectedCategory != null)
+                                viewModel.foods.load(selectedCategory!!)
                         }
-                        LazyColumn(
-                            state = listState,
-                            modifier = Modifier
-                                .padding(horizontal = horizontalPadding)
-                                .fillMaxWidth(),
-                        ) {
-                            items((1..50).toList()) { item ->
-                                Text(modifier = Modifier, text = "Item $item")
+
+
+                        HorizontalButtonSelection(
+                            values = categories.map { it.name },
+                            modifier = Modifier.padding(bottom = 5.dp),
+                            onChange = {selected ->
+                                selectedCategory = categories.first{it.name == selected}
+                                foodsLoading = true
+                                listState.dispatchRawDelta(Float.NEGATIVE_INFINITY)
+                                
+                            }
+                        )
+
+                        if(categoriesLoading || foodsLoading) {
+                            Box(
+                                modifier = Modifier.fillMaxSize()
+                            ){
+                                CircularProgressIndicator(
+                                    modifier = Modifier.align(Alignment.Center)
+                                )
+                            }
+                        }
+                        else{
+                            LazyColumn(
+                                state = listState,
+                                modifier = Modifier
+                                    .padding(horizontal = horizontalPadding)
+                                    .fillMaxWidth(),
+                            ) {
+                                items(foods.toList()) { item ->
+                                    FoodCard(item)
+                                }
                             }
                         }
                     }
@@ -135,9 +189,10 @@ fun HorizontalButtonSelection(
     selectedInitial: Int = 0,
     onChange: (newValue: String) -> Unit,
 ){
-    var selected by remember {
-        mutableStateOf(values[selectedInitial])
+    var selected by remember(values) {
+        mutableStateOf(values.getOrNull(selectedInitial)?.apply(onChange) ?: "")
     }
+
     val spacing = 10.dp
     Surface(
         modifier = modifier,
@@ -148,11 +203,15 @@ fun HorizontalButtonSelection(
         ){
             items(values){
                 Button(
-                    modifier = Modifier
-                        .width(95.dp)
-                        .selectable(it == selected, onClick = {}),
-                    colors = ButtonDefaults.filledTonalButtonColors(),
+                    modifier = Modifier,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = if(selected == it) MaterialTheme.colorScheme.primary
+                            else MaterialTheme.colorScheme.background,
+                        contentColor = if(selected == it) MaterialTheme.colorScheme.background
+                            else MaterialTheme.colorScheme.onBackground,
+                    ),
                     shape = RoundedCornerShape(8.dp),
+                    elevation = ButtonDefaults.buttonElevation(3.dp),
                     onClick = {
                         selected = it
                         onChange(it)
@@ -160,6 +219,57 @@ fun HorizontalButtonSelection(
                 ) {
                     Text(it)
                 }
+            }
+        }
+    }
+
+}
+
+@Composable
+fun FoodCard(
+    info: FoodCard,
+){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+            .padding(vertical = 20.dp, horizontal = 12.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        SubcomposeAsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(info.imageUrl)
+                .crossfade(true)
+                .build(),
+            loading = {CircularProgressIndicator()},
+            error = {Icons.Default.Photo},
+            contentDescription = info.name,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .clip(RoundedCornerShape(10.dp))
+                .fillMaxHeight()
+                .aspectRatio(1f)
+        )
+        Spacer(modifier = Modifier.size(20.dp))
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = info.name,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = info.description,
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            OutlinedButton(
+                modifier = Modifier.align(Alignment.End),
+                colors = ButtonDefaults.outlinedButtonColors(),
+                shape = RoundedCornerShape(8.dp),
+//                elevation = ButtonDefaults.buttonElevation(3.dp),
+                onClick = {},
+            ) {
+                Text(info.price.toString())
             }
         }
     }
@@ -180,6 +290,26 @@ fun SpecialOfferScroll(
             SpecialOfferCard()
         }
     }
+}
+
+@Composable
+fun LockScreenOrientation(orientation: Int) {
+    val context = LocalContext.current
+    DisposableEffect(orientation) {
+        val activity = context.findActivity() ?: return@DisposableEffect onDispose {}
+        val originalOrientation = activity.requestedOrientation
+        activity.requestedOrientation = orientation
+        onDispose {
+            // restore original orientation when view disappears
+            activity.requestedOrientation = originalOrientation
+        }
+    }
+}
+
+fun Context.findActivity(): Activity? = when (this) {
+    is Activity -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else -> null
 }
 
 @Composable
